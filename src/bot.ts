38195f8460d7412fc8736b6999fe092cfc681518
negatron99 <negatron99@gmail.com>
2019-06-30 13:32:07 +0100

    Getting bot to work using typescript

--- src/bot.ts
+++ src/bot.ts
@@ -8,55 +8,48 @@ import {format} from "winston";
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console({
-    level: 'debug',
-    format: format.combine(
-      format.timestamp(),
-      format.colorize(),
-      format.simple()
-    )}));
+  level: 'debug',
+  format: format.combine(
+    format.timestamp(),
+    format.colorize(),
+    format.simple()
+  )
+}));
 
 // Initialize Discord Bot
 const bot = new Client();
 
 let prefix = '!';
 
-bot.on('ready',  () => {
-    const {user} = bot;
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(user.username + ' - (' + user.id + ')');
+bot.on('ready', () => {
+  const {user} = bot;
+  logger.info('Connected');
+  logger.info('Logged in as: ');
+  logger.info(user.username + ' - (' + user.id + ')');
 
-    prefix = `<@${user.id}>`;
+  prefix = `<@${user.id}>`;
 
-    logger.info(`Setting prefix to '${prefix}'`);
+  logger.info(`Setting prefix to '${prefix}'`);
 });
 bot.on('message', (message) => {
 
-    console.log(message);
+  const {content, channel} = message;
 
-    /*
+  // Our bot needs to know if it will execute a command
+  // It will listen for messages that will start with `!`
+  if (content.substring(0, prefix.length) === prefix) {
+    let [cmd, ...args] = content.substring(prefix.length).trim().split(' ');
 
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, prefix.length) === prefix) {
-        let [cmd, ...args] = message.substring(prefix.length).trim().split(' ');
-
-        switch (cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channel,
-                    message: 'Pong!'
-                });
-                break;
-            default:
-                bot.sendMessage({
-                    to: channel,
-                    message: '_meow_'
-                })
-        }
+    switch (cmd) {
+      // !ping
+      case 'ping':
+        channel.send('Pong!');
+        break;
+      default:
+        channel.send('_meow_');
     }
-     */
+  }
 });
 
 bot.login(process.env.BOT_TOKEN);
+ 
